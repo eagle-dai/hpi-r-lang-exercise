b38e2cd612d39cd8cb942568d6001922d33258ae
Eagle Dai <dynuaa@gmail.com>
2023-01-24 23:16:22 +0800

    r1.4 - 原题

---
+++ R1.4_Exercise/FP14_Exercises/FP14_Exercises_1.R
@@ -0,0 +1,29 @@
+# T1 ----
+# Subtract 7 from 49. It is enough to simply write the code for it.
+
+
+# In Rstudio: with the mouse cursor on the solution line, 
+# press CTRL + Enter (or click 'Run' in the upper right corner).
+
+
+# If the result seems correct, have it checked by the hidden test script
+# by running the next line (or clicking 'Source').
+codeoceanR::rt_score()
+# Ignore messages about tasks you haven't worked on yet.
+# (they indicate how much work is left).
+
+
+# T2 ----
+# Multiply 21 with 2.
+
+
+
+# Score again now to have your solution checked.
+# It is worth doing this at least once after each task!!!
+
+# In Rstudio: CTRL + SHIFT + S to call 'source' without mouse clicks.
+# This will run the whole script including rt_score().
+
+# Run score often to find errors easily!!!
+
+# Now continue in "FP14_Exercises_2.R"
---
+++ R1.4_Exercise/FP14_Exercises/FP14_Exercises_2.R
@@ -0,0 +1,16 @@
+codeoceanR::rt_score()
+# See course video / PDF lesson 1.4 Exercises
+
+# T3 ----
+# Divide 294 by 7.
+
+
+# T4 ----
+# Multiply 12 with 3,5.
+# Tip: In programming, decimal numbers are not represented with a comma. 
+
+
+# If you want to see your score on openHPI, submit it with:
+# codeoceanR::rt_submit()
+# This is optional, but openHPI provides a nice progress overview:
+# https://open.hpi.de/courses/hpi-dh-fprog2022/progress (SHIFT + click in Rstudio)
